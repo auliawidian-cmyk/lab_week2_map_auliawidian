a2119c5fb0e4c3162d02fc80cd0843f531a61f93
ul.auliaaa <163988764+auliaaaaidr@users.noreply.github.com>
2025-09-08 15:49:25 +0700

    Commit No. 1 – Activity lifecycle

---
+++ app/src/androidTest/java/com/example/labmap_week2_auliawidianputri_11317o0/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.labmap_week2_auliawidianputri_11317o0
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.labmap_week2_auliawidianputri_11317o0", appContext.packageName)
+    }
+}
